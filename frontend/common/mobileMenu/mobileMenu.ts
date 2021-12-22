a53dca2815d20a87fda47d4f617b616a316e1335
qiud <1726015433@qq.com>
2021-12-22 21:50:33 +0800

    '1'

--- frontend/common/mobileMenu/mobileMenu.ts
+++ frontend/common/mobileMenu/mobileMenu.ts
@@ -1,6 +1,13 @@
 import * as $ from 'jquery';
 
 $('.mobileMenu__item').on('click',(e)=>{
-  $('.mobileMenu__item').removeClass('mobileMenu__item--active');
-  $(e.currentTarget).addClass('mobileMenu__item--active');
+  // $('.mobileMenu__item').removeClass('mobileMenu__item--active');
+  $(".mobileMenu__item").not(this).removeClass('mobileMenu__item--active');
+  const $current = $(e.currentTarget);
+  if($current.hasClass('mobileMenu__item--active')){
+    $current.removeClass('mobileMenu__item--active');
+  }else{
+    $current.addClass('mobileMenu__item--active');
+  }
+  
 });
