4f267f222df062590510cc939f8684ed36364be0
qiud <1726015433@qq.com>
2022-04-26 16:17:32 +0800

    '2022426'

--- inc/q2/api/v1/Q2PageRouter.php
+++ inc/q2/api/v1/Q2PageRouter.php
@@ -6,17 +6,10 @@ use hedao\core\BaseRouter;
 use hedao\core\TSingleton;
 use hedao\dao\PostDao;
 use hedao\lib\constant\MetaBoxOptions;
-use hedao\lib\exceptions\DeletePostFailed;
-use hedao\lib\exceptions\DeletePostListFailed;
-use hedao\lib\exceptions\InsertPostFailed;
-use hedao\lib\exceptions\Success;
-use hedao\lib\exceptions\UpdatePostFailed;
 use hedao\lib\GetMenuData;
 use q2\lib\constant\Constants;
 use q2\lib\constant\Options;
 
-use function hedao\lib\helper\getGETValue;
-use function hedao\lib\helper\getPOSTValue;
 
 
 class Q2PageRouter extends BaseRouter{
@@ -87,104 +80,6 @@ class Q2PageRouter extends BaseRouter{
 
 
 
-  
-
-
-  
-  
-  /**
-   * //1. 查询条件
-   * 标题或内容 titleOrContent
-   * 分类id categoryId 
-   * 标签id tagId 
-   * 作者id authorId 
-   * 文章状态 status 'publish'|'private'
-   * //2. 排序条件
-   * 排序条件  orderBy  'create_time'|'update_time'|'comment_count'|'rand'
-   * 排序规则 orderRule 'DESC'|'ASC'
-   * //3. 分页
-   * page size
-   */
-  public function queryListRouter($request){
-    //1. 获取查询条件
-    $titleOrContent = getGETValue('titleOrContent');
-    $categoryId = getGETValue('categoryId');
-    $tagId = getGETValue('tagId');
-    $authorId = getGETValue('authorId');
-    $status = getGETValue('status',['publish']); //有默认
-
-    $orderBy = getGETValue('orderBy','create_time');//有默认
-    $orderRule = getGETValue('orderRule','DESC');//有默认
-
-    $page = getGETValue('page',1);//有默认
-    $size = getGETValue('size',10);//有默认
-    //2. 消毒
-    if($categoryId){
-      $categoryIdListIn = [$categoryId];
-    }
-    if($tagId){
-      $tagIdListIn = [$tagId];
-    }
-    if($authorId){
-      $authorIdListIn = [$authorId];
-    }
-    if($titleOrContent){
-      $s = $titleOrContent;
-    }
-    //3. 
-    $listAndCount = PostDao::queryPostList(
-      ['categoryIdListIn'=>@$categoryIdListIn],
-      ['tagIdListIn'=>@$tagIdListIn],
-      ['authorIdListIn'=>@$authorIdListIn],
-      ['postType'=>'post'],
-      @$s,
-      $status,
-      $orderBy,
-      $orderRule,
-      $page,
-      $size,
-      ['author','category','tag','meta'],
-      [
-        MetaBoxOptions::HEDAO_OUTSIDE_THUMBNAIL_URL,
-
-        MetaBoxOptions::HEDAO_COMMON_DESCRIPTION,
-        MetaBoxOptions::HEDAO_COMMON_KEYWORDS,
-        MetaBoxOptions::HEDAO_COMMON_VIEW_COUNT,
-
-        MetaBoxOptions::HEDAO_DOWNLOAD_URL,
-        MetaBoxOptions::HEDAO_DOWNLOAD_PASSWORD,
-        MetaBoxOptions::HEDAO_DOWNLOAD_UNPACK_PASSWORD,
-      ]
-    );
-    return $listAndCount;
-  }
-
-  public function deleteOneRouter($request){
-    $id = getPOSTValue('id');
-    if(!$id){
-      return new DeletePostListFailed('必须传入id');
-    }
-    $res = PostDao::deleteOnePost($id);
-    if($res == 0){
-      return new DeletePostFailed();
-    }
-    return new Success();
-  }
-
-
-  public function deleteManyRouter($request){
-    $idArr = getPOSTValue('idArr');
-    if(!$idArr || count($idArr) === 0){
-      return new DeletePostListFailed('必须传入idArr');
-    }
-    $res = deletePostListByIdArr($idArr);
-    $success = $res['success'];
-    $failed = $res['failed'];
-    if($failed !== 0){
-      return new DeletePostListFailed('删除成功了' . $success .' 篇文章, 删除失败了 '. $failed . ' 篇文章' );
-    }
-    return new Success();
-  }
 
 
 
